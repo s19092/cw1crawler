0fcdcc84a5e65530e0d7da7724133d0327c34ab4
izikodzik <ITI-guuy@gmail.com>
2020-03-08 22:05:04 +0100

    Zrobione

---
+++ krawler/krawler/Program.cs
@@ -0,0 +1,41 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace krawler
+{
+    class Program
+
+
+    {
+
+        static async System.Threading.Tasks.Task Main(string[] args)
+        {
+
+            var httpClient = new HttpClient();
+            var response = await httpClient.GetStringAsync(args[0]);
+            var responseAsString = response.ToString();
+
+            Console.WriteLine(responseAsString);
+            Console.WriteLine("\n");
+
+            Regex exp = new Regex("\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b", RegexOptions.IgnoreCase);
+            MatchCollection emails = exp.Matches(response);
+            HashSet<String> setMail = new HashSet<string>();
+
+            foreach (Match email in emails) {
+                setMail.Add(email.Value.ToUpper());
+             
+              
+            }
+
+            foreach (String mail in setMail)
+                Console.WriteLine(mail);
+
+        }
+
+    }
+}
